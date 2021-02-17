dc7df370c2206360bfdb10843d35a068bb6bb0f2
Brandon Thomas <65044669+thombran@users.noreply.github.com>
2021-02-16 21:06:53 -0500

    Completed checkWInner and isBoardFull functions. Currently about halfway done with isValidMove function.

--- Othello.js
+++ Othello.js
@@ -1,10 +1,8 @@
 'use strict';
-
 /**
  * Othello game
  * Author(s): Brandon Thomas
  */
-
 class Othello {
 
 	// Constructs and initializes the board of given size
@@ -46,34 +44,34 @@ class Othello {
 	}
 
 	// Getter for white disc
-  	static get WHITE() {
-    	return "W";
-  	}
+	static get WHITE() {
+		return "W";
+	}
 
-  	// Getter for black disc
-  	static get BLACK() {
-    	return "B";
-  	}
+	// Getter for black disc
+	static get BLACK() {
+		return "B";
+	}
 
-  	// Getter for empty
-  	static get EMPTY() {
-    	return "-";
-  	}
+	// Getter for empty
+	static get EMPTY() {
+		return "-";
+	}
 
-  	// Getter for tie
-  	static get TIE() {
-    	return "T";
-  	}
+	// Getter for tie
+	static get TIE() {
+		return "T";
+	}
 
-  	// Initializes the board with start configuration of discs (as per project specs)
+	// Initializes the board with start configuration of discs (as per project specs)
 	initializeBoard() {
 		console.log(this.size);
 		for (let row = 0; row < this.size; row++) {
 			for (let col = 0; col < this.size; col++) {
-				if ((row == ((this.size / 2) -1) && col == (this.size / 2) - 1 || (row == this.size / 2) && col == this.size / 2)) {
+				if ((row == ((this.size / 2) - 1) && col == (this.size / 2) - 1 || (row == this.size / 2) && col == this.size / 2)) {
 					this.board[row][col] = Othello.BLACK;
 				}
-				else if (((row == this.size / 2) && col == (this.size / 2) - 1) || row == (this.size / 2) -1 && col == this.size / 2) {
+				else if (((row == this.size / 2) && col == (this.size / 2) - 1) || row == (this.size / 2) - 1 && col == this.size / 2) {
 					this.board[row][col] = Othello.WHITE;
 				}
 				else {
@@ -82,21 +80,111 @@ class Othello {
 			}
 		}
 
-  	}
+	}
 
 	// Returns true if placing the disc of current player at row,col is valid; else returns false
 	isValidMove(row, col) {
 		return this.isValidMoveForDisc(row, col, this.disc);
 	}
 
+	checkNorth(row, col, disc) {
+		var numDisc = 0;
+		var numNotDisc = 0;
+		var numEmpty = 0;
+
+		for (let i = row + 1; i < this.size; i++) {
+			if (i == size)
+				break;
+			if (this.board[i][col] === disc) {
+				numDisc++;
+				if (numDisc > 0 && numNotDisc == 0 || numEmpty > 0)
+					return false;
+				if (numNotDisc > 0 && numDisc > 0)
+					return true;
+			}
+			if (this.board[i][col] != disc && board[i][col] !== Othello.EMPTY)
+				numNotDisc++;
+			if (board[i][col] === Othello.EMPTY)
+				numEmpty++;
+		}
+		return false;
+	}
+
+	checkSouth(row, col, disc) {
+		var numDisc = 0;
+		var numNotDisc = 0;
+		var numEmpty = 0;
+
+		for (let i = row - 1; i >= 0; i--) {
+			if (i < 0)
+				break;
+			if (this.board[i][col] === disc) {
+				numDisc++;
+				if (numDisc > 0 && numNotDisc == 0 || numEmpty > 0)
+					return false;
+				if (numNotDisc > 0 && numDisc > 0)
+					return true;
+			}
+			if (this.board[i][col] != disc && board[i][col] !== Othello.EMPTY)
+				numNotDisc++;
+			if (board[i][col] === Othello.EMPTY)
+				numEmpty++;
+		}
+		return false;
+	}
+
+	checkEast(row, col, disc) {
+		var numDisc = 0;
+		var numNotDisc = 0;
+		var numEmpty = 0;
+
+		for (let i = col + 1; i < this.size; i++) {
+			if (i == size)
+				break;
+			if (this.board[row][i] === disc) {
+				numDisc++;
+				if (numDisc > 0 && numNotDisc == 0 || numEmpty > 0)
+					return false;
+				if (numNotDisc > 0 && numDisc > 0)
+					return true;
+			}
+			if (this.board[row][i] != disc && board[row][i] !== Othello.EMPTY)
+				numNotDisc++;
+			if (board[row][i] === Othello.EMPTY)
+				numEmpty++;
+		}
+		return false;
+	}
+
+	checkWest(row, col, disc) {
+		var numDisc = 0;
+		var numNotDisc = 0;
+		var numEmpty = 0;
+
+		for (let i = col - 1; i >= 0; i--) {
+			if (i < 0)
+				break;
+			if (this.board[row][i] === disc) {
+				numDisc++;
+				if (numDisc > 0 && numNotDisc == 0 || numEmpty > 0)
+					return false;
+				if (numNotDisc > 0 && numDisc > 0)
+					return true;
+			}
+			if (this.board[row][i] != disc && board[row][i] !== Othello.EMPTY)
+				numNotDisc++;
+			if (board[row][i] === Othello.EMPTY)
+				numEmpty++;
+		}
+		return false;
+	}
+
 	// Returns true if placing the specified disc at row,col is valid; else returns false
 	isValidMoveForDisc(row, col, disc) {
-
 		// TO DO: COMPLETE THIS PART OF THE METHOD
 
-
 		// DO NOT DELETE - if control reaches this statement, then it is not a valid move
-		return false;	// not a valid move
+		return false; // not a valid move
 	}
 
 	// Places the disc of current player at row,col and flips the opponent discs as needed
@@ -110,8 +198,6 @@ class Othello {
 
 
 		// TO DO: COMPLETE THIS PART OF THE METHOD
-
-
 		// DO NOT DELETE - prepares for next turn if game is not over
 		if (!this.isGameOver()) {
 			this.prepareNextTurn();
@@ -139,50 +225,75 @@ class Othello {
 
 	// Returns true if a valid move for the specified disc is available; else returns false
 	isValidMoveAvailableForDisc(disc) {
-
 		// TO DO: COMPLETE THIS PART OF THE METHOD
 
-		
 		// DO NOT DELETE - if control reaches this statement, then a valid move is not available
-		return false;	// not a valid move
+		return false; // not a valid move
 	}
 
 	// Returns true if the board is fully occupied with discs; else returns false
 	isBoardFull() {
-
-		// TO DO: COMPLETE THIS PART OF THE METHOD
-
+		//Go row by row and col by col and check to see if there is any empty board spots. If there is, board is not full (false)
+		for (let row = 0; row < this.size; row++) {
+			for (let col = 0; col < this.size; col++) {
+				if (this.board[row][col] === Othello.EMPTY) {
+					return false;
+				}
+			}
+		}
 		return true;
 	}
 
 	// Returns true if either the board is full or a valid move is not available for either disc
 	isGameOver() {
 		return this.isBoardFull() ||
-					(!this.isValidMoveAvailableForDisc(Othello.WHITE) &&
-								!this.isValidMoveAvailableForDisc(Othello.BLACK));
+			(!this.isValidMoveAvailableForDisc(Othello.WHITE) &&
+				!this.isValidMoveAvailableForDisc(Othello.BLACK));
 	}
 
 	// If there is a winner, it returns Othello.WHITE or Othello.BLACK.
 	// In case of a tie, it returns Othello.TIE
 	checkWinner() {
+		var numBlack = 0; //Number of white pieces on the board
+		var numWhite = 0; //Number of black pieces on the board
 
-		// TO DO: COMPLETE THIS METHOD
-		
+
+		//Go row by row and col by col and check what piece is in each section of the board, increment counts
+		for (let row = 0; row < this.size; row++) {
+			for (let col = 0; col < this.size; col++) {
+				if (this.board[row][col] === Othello.BLACK) {
+					numBlack++;
+				}
+				else if (this.board[row][col] === Othello.WHITE) {
+					numWhite++;
+				}
+			}
+		}
+		// If more black pieces than white, return BLACK, more white than black return WHITE, otherwise it's a TIE
+		if (numBlack > numBlack) {
+			return Othello.BLACK;
+		}
+		if (numWhite > numBlack) {
+			return Othello.WHITE;
+		}
+		else {
+			return Othello.TIE;
+		}
 	}
 
 	// Returns a string representation of the board (for display purposes)
 	toString() {
 		let str = '\n ';
 		for (let i = 0; i < this.size; i++) {
-			str += ' ' + (i+1)
+			str += ' ' + (i + 1);
 		}
 		str += "\n";
 		for (let i = 0; i < this.size; i++) {
-			str += i+1 + ' ';
+			str += i + 1 + ' ';
 			str += this.board[i].join(' ') + "\n";
 		}
 		return str;
 	}
 }
 
-module.exports = Othello;
+exports.Othello = Othello;
